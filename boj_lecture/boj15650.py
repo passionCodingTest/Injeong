1380ce55bd842a1fa482d231556e8f64078db010
6v6 <injeong410@gmail.com>
2021-02-25 00:50:35 +0900

    N과 M (2)

--- boj_lecture/boj15650.py
+++ boj_lecture/boj15650.py
@@ -12,3 +12,20 @@ answer = list(combinations(data, m))
 
 for ans in answer:
     print(" ".join(map(str, ans)))
+
+'''
+answer_list = [0] * m
+def dfs(index, selected):
+    if selected == m:
+        print(" ".join(map(str, ans)))
+        return
+    
+    if index > n:
+        return
+    answer_list[selected] = index
+    dfs(index + 1, selected + 1)
+    answer_list[selected] = 0
+    dfs(index + 1, selected)
+
+dfs(1, 0)
+'''

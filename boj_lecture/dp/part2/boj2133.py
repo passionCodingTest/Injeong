d80ed9e2a26cf31fd22c11ef0f3b2bb2f9277ef2
6v6 <injeong410@gmail.com>
2021-03-26 16:32:06 +0900

    타일 채우기

---
+++ boj_lecture/dp/part2/boj2133.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 31
+dp[2] = 3
+for i in range(4, 31, 2):
+    dp[i] = dp[2] * dp[i-2]
+    for j in range(4, i, 2):
+        dp[i] += 2*dp[i-j]
+    dp[i] += 2
+    
+print(dp[n])
